e734ad8b06a8577e2e1ffc096f817ce413f2c453
Maksood Mohiuddin <maksood@gmail.com>
2014-05-02 16:35:56 -0400

    Adding to github

---
+++ WordSearch/WordSearch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace WordSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // WordSearch.txt and WordList.txt are embedded resources in this assembly, load these files in stream readers 
+                var assembly = Assembly.GetExecutingAssembly();
+                var wordSearchStreamReader = new StreamReader(assembly.GetManifestResourceStream("WordSearch.WordSearch.txt"));
+                var wordListStreamReader = new StreamReader(assembly.GetManifestResourceStream("WordSearch.WordList.txt"));
+
+
+                Console.WriteLine(wordSearchStreamReader.ReadToEnd());
+                Console.WriteLine(wordListStreamReader.ReadToEnd());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error accessing resources" + ex.ToString());
+            }
+
+            // exit the console app     
+            Console.Write("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
